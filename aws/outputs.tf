0e3cdd38284d1e195bda74d3b0cd6639f2fc39b8
guirgouveia <124064209+guirgouveia@users.noreply.github.com>
2023-10-30 03:46:27 -0300

    Update AWS modules

--- aws/outputs.tf
+++
@@ -1,5 +0,0 @@
-# Define the output variables for the module
-output "kubeconfig" {
-  description = "The kubectl configuration for the EKS cluster."
-  value       = module.eks.kubeconfig-certificate-authority-data
-}
